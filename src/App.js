6c94301621cb9398556415d17efe885c9b23138f
Sharmajay89 <sharmajayit89@gmail.com>
2023-08-07 17:43:13 +0530

    Add files via upload
    project code update fr store

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Navbar, Sidebar, Footer } from './components'
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+  AuthWrapper,
+} from './pages'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <p>
-          Ajaya !
-        </p>
-      </header>
-    </div>
-  );
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='cart' element={<Cart />} />
+          <Route path='products' element={<Products />} />
+          <Route path='products/:id' element={<SingleProduct />} />
+          <Route
+            path='checkout'
+            element={
+              <PrivateRoute>
+                <Checkout />
+              </PrivateRoute>
+            }
+          />
+          <Route path='error' element={<Error />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </AuthWrapper>
+  )
 }
 
-export default App;
+export default App
